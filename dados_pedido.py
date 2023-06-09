9dd7b997b8fb3002e199149b3cf8a758975d0898
Julio Teixeira <julioteixeira700@gmail.com>
2023-06-09 15:33:08 -0300

    Adiciona script para criar a estrutura JSON do pedido.

---
+++ dados_pedido.py
@@ -0,0 +1,29 @@
+nome = "Teste cliente 4"
+
+cpf_cnpj = "22755775426"
+
+desconto = "28.73"
+
+itens = [
+    {
+        "codigo": "5602660004319",
+        "descricao": "CEREMONY TAWNY 10 ANOS 50ML",
+        "unidade": "UN",
+        "quantidade": "1",
+        "valor_unitario": "24.90"
+    },
+    {
+        "codigo": "5602012602347",
+        "descricao": "TERRAS DE XISTO TN 750ML",
+        "unidade": "UN",
+        "quantidade": "1",
+        "valor_unitario": "54.90"
+    },
+    {
+        "codigo": "7804452006305",
+        "descricao": "3 ORIGENES TN 750ML BLEND",
+        "unidade": "UN",
+        "quantidade": "1",
+        "valor_unitario": "210.00"
+    }
+]
